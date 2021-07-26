c424af0e61375062c14cd729bd729518f97e4afa
kmuni08 <Karishma.Muni24@gmail.com>
2021-07-26 15:07:32 -0400

    Resolved issues in add, when adding to wishlist. Also created API endpoint for delete and created views for summary of wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -6,13 +6,34 @@ from django.http import JsonResponse
 # Create your views here.
 
 def wishlist_summary(request):
-    return render(request, 'wishlist/summary.html')
+    wishlist = Wishlist(request)
+    return render(request, 'wishlist/summary.html', {'wishlist': wishlist})
 
 def wishlist_add(request):
     wishlist = Wishlist(request)
     if request.POST.get('action') == 'POST':
         recipe_id = int(request.POST.get('recipeId'))
+        recipe_qty = 1
         recipe = get_object_or_404(Recipe, id=recipe_id)
-        wishlist.add(recipe = recipe)
-        response = JsonResponse({'test': 'data'})
+        # if recipe_id not in wishlist:
+        #     wishlist.add(recipe = recipe, qty = recipe_qty)
+        wishlist.add(recipe = recipe, qty = recipe_qty)
+        # response = JsonResponse({'test': 'data'})
+        # wishlistqty = wishlist.__len__()
+        wishlistqty = wishlist.__len__()
+        response = JsonResponse({'qty': wishlistqty})
         return response
+
+def wishlist_delete(request):
+    wishlist = Wishlist(request)
+    if request.POST.get('action') == 'POST':
+        print("recipe_id", request.POST.get('recipeId'))
+        
+        recipe_id = int(request.POST.get('recipeId'))
+    
+        wishlist.delete(recipe=recipe_id)
+
+        wishlistqty = wishlist.__len__()
+        response = JsonResponse({'qty': wishlistqty})
+        return response
+
